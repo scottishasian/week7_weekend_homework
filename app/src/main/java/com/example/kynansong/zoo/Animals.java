95a16eaf5a1321b2902ff85ab09306444a3cfd99
Kynan <k.song@blueyonder.co.uk>
2017-11-10 10:41:34 +0000

    Created Zoo class

---
+++ app/src/main/java/com/example/kynansong/zoo/Animals.java
@@ -0,0 +1,16 @@
+package com.example.kynansong.zoo;
+
+/**
+ * Created by kynansong on 10/11/2017.
+ */
+
+public abstract class Animals {
+    private String name;
+    private double value;
+
+    public Animals(String name, double value) {
+        this.name = name;
+        this.value = value;
+
+    }
+}
