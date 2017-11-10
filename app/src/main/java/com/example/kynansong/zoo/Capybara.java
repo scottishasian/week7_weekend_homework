6de45d77f03444db0f4ff5fe5002a8d2f198c204
Kynan <k.song@blueyonder.co.uk>
2017-11-10 11:09:29 +0000

    Added 2 more animal subclasses.

---
+++ app/src/main/java/com/example/kynansong/zoo/Capybara.java
@@ -0,0 +1,11 @@
+package com.example.kynansong.zoo;
+
+/**
+ * Created by kynansong on 10/11/2017.
+ */
+
+public class Capybara extends Animals {
+    public Capybara(String name, double value) {
+        super(name, value);
+    }
+}
---
+++ app/src/main/java/com/example/kynansong/zoo/ShoebillStork.java
@@ -0,0 +1,11 @@
+package com.example.kynansong.zoo;
+
+/**
+ * Created by kynansong on 10/11/2017.
+ */
+
+public class ShoebillStork extends Animals {
+    public ShoebillStork(String name, double value) {
+        super(name, value);
+    }
+}
