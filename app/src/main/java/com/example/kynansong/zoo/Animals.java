ebad9250dc0706adb355a871e3ffc9d92c686701
Kynan <k.song@blueyonder.co.uk>
2017-11-10 10:49:13 +0000

    Created getter and setter methods for Animals.

--- app/src/main/java/com/example/kynansong/zoo/Animals.java
+++ app/src/main/java/com/example/kynansong/zoo/Animals.java
@@ -13,4 +13,20 @@ public abstract class Animals {
         this.value = value;
 
     }
+
+    public String getAnimalName() {
+        return this.name;
+    }
+
+    public double getAnimalValue() {
+        return this.value;
+    }
+
+    public void setAnimalName(String name){
+        this.name = name;
+    }
+
+    public void setAnimalValue(double value){
+        this.value = value;
+    }
 }
